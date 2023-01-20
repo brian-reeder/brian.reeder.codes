b781ecb152430d7637bdff13f69f16e9c87875fc
@brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2023-01-20 16:19:40 -0600

    Add 404 plugin

--- scully.brian.reeder.codes.config.ts
+++ scully.brian.reeder.codes.config.ts
@@ -1,10 +1,13 @@
 import { ScullyConfig } from '@scullyio/scully';
+import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
+
+const Http404Plugin = getHttp404Plugin();
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'brian.reeder.codes',
   distFolder: './dist/brian.reeder.codes', // output directory of your Angular build artifacts
   outDir: './dist/static', // directory for scully build artifacts
-  defaultPostRenderers: [],
+  defaultPostRenderers: [Http404Plugin],
   routes: {},
 };
