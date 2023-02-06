ab77e9980478b5c9fc4829ffeebeb5d99b51ec21
Brian <14955584+brian-reeder@users.noreply.github.com>
2023-02-05 18:55:38 -0600

    Add devcontainer (#17)

--- scully.brian.reeder.codes.config.ts
+++ scully.brian.reeder.codes.config.ts
@@ -21,4 +21,21 @@ export const config: ScullyConfig = {
       },
     },
   },
+  puppeteerLaunchOptions: {
+    args: [
+      "--disable-gpu",
+      "--renderer",
+      "--no-sandbox",
+      "--no-service-autorun",
+      "--no-experiments",
+      "--no-default-browser-check",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions"
+    ],
+    executablePath: '/usr/bin/chromium-browser'
+  }
 };
